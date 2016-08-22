497c9537964cb18b70c2d76b37ef5ee30f3b3cbc
叶帆 <yeziahehe@gmail.com>
2016-08-22 19:13:47 +0800

    优化：表格样式优化

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -14,4 +14,25 @@
         $('.exit-mask').css('display', 'none');
     });
 
+    $("#user").DataTable({
+        "info": false,
+        "lengthChange": false,
+        /*"sPaginationType": "full_numbers",*/
+        "oLanguage": {
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+            "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+            "sInfoEmpty": "没有数据",
+            "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+            "sZeroRecords": "没有检索到数据",
+            "sSearch": "",
+            "sSearchPlaceholder": "搜索",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "&laquo;",
+                "sNext": "&raquo;",
+                "sLast": "尾页"
+            }
+        }
+    });
+
 })();
